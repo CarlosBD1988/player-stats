de63dbf5137f2815f9f3ceddc90106c129bc8d75
Carlos Bohorquez <carlos.bohorquez@agilityrpa.com>
2025-01-21 13:08:46 -0500

    Change welcome message

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <h1>¡Bienvenido a Player Stats!</h1>
+      <h1>WELCOME TO PLAYER STATS!</h1>
       <img src="https://media2.giphy.com/media/dWrDicEgHO52f9k8Du/200.webp?cid=ecf05e47vxd5ovvlupp59z6eqy4vtb0favdyh7k24ganvucf&ep=v1_gifs_related&rid=200.webp&ct=g" alt="Decorativo" className="home-gif" />
       
     </div>
